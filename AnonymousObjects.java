a7f7a408a93e4b2a8d364dabd521380096f22953
Nagaraju.Kondru <nagaraju.kondru22@gmail.com>
2025-08-11 16:22:33 +0530

    Create AnonymousObjects.java
    anonymous objects are objects that are created without assigning them to any reference variable.
    
    They are mainly used once and then immediately become eligible for garbage collection.

---
+++ AnonymousObjects.java
@@ -0,0 +1,9 @@
+
+class AnonymousObjects{
+    public void display(){
+        System.out.println("anonyonms object");
+    }
+    public static void main(String args[]){
+        new AnonymousObjects().display();
+    }
+}
