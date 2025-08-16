5cc8f1ec5dfd15b7ba5642b5e429e2aedecee740
Nagaraju.Kondru <nagaraju.kondru22@gmail.com>
2025-08-16 15:25:34 +0530

    Delete MCQ-Quiz-Application directory

--- MCQ-Quiz-Application/Home.java
+++
@@ -1,7 +0,0 @@
-public class Home{
-    public static void main(String args[]){
-        QuestionService questionService = new QuestionService();
-        questionService.startExam();
-        questionService.scoreExam();
-    }
-}
