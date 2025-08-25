9d6694669b193e31ffe4c1b9a97a38d1ee6da1a9
Nagaraju.Kondru <nagaraju.kondru22@gmail.com>
2025-08-25 19:00:37 +0530

    Throwing a built-in exception (throw)

---
+++ 2.4 ExceptionHandling.java
@@ -0,0 +1,11 @@
+class ExceptionHandling{
+    public static void main(String args[]){
+        int age=15;
+        if(age>18){
+           System.out.println("Eligible to vote..");
+        }
+        else{
+             throw new ArithmeticException("Not aligible ?.");
+        }
+    }
+}
