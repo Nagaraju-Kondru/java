4118d7e4303c14009fa7577e98ae345b2f8f5585
Nagaraju.Kondru <nagaraju.kondru22@gmail.com>
2025-08-29 22:55:34 +0530

    Sum of elements in a given array

---
+++ Java_Coding_Problems/ArraySum.java
@@ -0,0 +1,27 @@
+// Online Java Compiler
+
+import java.util.*;
+class ArraySum {
+    static int sum=0;//instance variable share to main and fun
+    public static void main(String[] args) {
+       Scanner obj=new Scanner(System.in);
+       System.out.println("Enter the array size: ");
+       int n=obj.nextInt();
+       int len=n-1;
+       System.out.println("Enter Array Values: ");
+       int arr[]=new int[n];
+       for(int i=0;i<n;i++){
+           arr[i]=obj.nextInt();
+       }   
+       
+       fun(len,arr);
+       System.out.println("sum: "+sum);
+    }
+    static void fun(int len,int arr[]){
+        if(len<0){
+            return;
+        }
+        sum+=arr[len];
+        fun(len-1,arr);
+    }
+}
