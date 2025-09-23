3ac5df800bfcb75ee834331e4f4cf10c31331df9
Nagaraju.Kondru <nagaraju.kondru22@gmail.com>
2025-09-23 18:38:02 +0530

    Array Left Rotate Only one time

---
+++ Java_Coding_Problems/Array_LeftRotate.java
@@ -0,0 +1,13 @@
+class Array_LeftRotate{
+    public static void main(String args[]){
+        int a[]={1,2,4,5,6};
+        int temp=a[0];
+        for(int i=1;i<a.length;i++){
+            a[i-1]=a[i];
+        }
+        a[a.length-1]=temp;
+        for(int i=0;i<a.length;i++){
+            System.out.print(a[i]+" ");
+        }
+    }
+}
