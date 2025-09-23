3383242cc81cba8fc625a56ce1008bffcd6302fa
Nagaraju.Kondru <nagaraju.kondru22@gmail.com>
2025-09-23 17:47:32 +0530

    Find first and second largest element

---
+++ Java_Coding_Problems/Array_SecondLargest.java
@@ -0,0 +1,28 @@
+class Array_SecondLargest{
+    public static void main(String args[]){
+        int a[]={40,60,60,50,10,1,1,2,40,10,20,9,0,50,5,2};
+        int first=a[0];
+        int second=a[1];
+        if(a[1]>a[0]){
+            first=a[1];
+            second=a[0];
+        }
+        
+        for(int i=2;i<a.length;i++){
+            if(a[i]>first){
+                second=first;
+                first=a[i];
+            }
+            else if(a[i]>second&&a[i]!=first){
+                second=a[i];
+            }
+        }
+        if(first==second){
+            System.out.println("no second largest element");
+        }
+        else{
+            System.out.println("first largest element: "+first);
+            System.out.println("Second largest element: "+second);
+        }
+    }
+}
