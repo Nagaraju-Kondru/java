c1812828dacd9714b93d95c102eacbe10ef72b47
Nagaraju.Kondru <nagaraju.kondru22@gmail.com>
2025-09-23 16:05:05 +0530

    Find All dublicate values in array

---
+++ Java_Coding_Problems/Array_All_Dublicates.java
@@ -0,0 +1,28 @@
+class Array_All_Dublicates{
+    public static void main(String args[]){
+        int a[]={1,1,2,2,3,4,5,6,4,3,2,1,1,3,2,11,11,5};
+        boolean bool[]=new boolean[a.length];
+        
+        for(int i=0;i<a.length;i++){
+            if(bool[i]){
+                continue;
+            }
+            int count=1;
+            for(int j=i+1;j<a.length;j++){
+                if(a[i]==a[j]){
+                    count++;
+                    bool[j]=true;
+                }
+                
+            }        
+         
+        }
+        System.out.println();
+        for(int i=0;i<a.length;i++){
+            if(bool[i]){
+                System.out.print(a[i]+" ,");
+            }
+         }
+        
+    }
+}
