9d9bbbc7c06fbd9a3f4a9d3997cbdad4d9352e27
Nagaraju.Kondru <nagaraju.kondru22@gmail.com>
2025-09-24 12:50:11 +0530

    Access the first and last element of an array.

---
+++ Java_Coding_Problems/Arrays/FisrtLastElement.java
@@ -0,0 +1,7 @@
+public class FisrtLastElement {
+    public static void main(String args[]){
+        int a[]={1,2,3,4,5,6};
+        System.out.println("First Element:"+a[0]);
+        System.out.println("Last Element:"+a[a.length-1]);
+    }
+}
