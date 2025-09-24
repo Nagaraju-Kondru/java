54552d3f86d0535ba9f2f3271143c07d9d77ff84
Nagaraju.Kondru <nagaraju.kondru22@gmail.com>
2025-09-24 12:41:59 +0530

    Print all elements of an array.

---
+++ Java_Coding_Problems/Arrays/PrintArray.java
@@ -0,0 +1,8 @@
+public class PrintArray {
+    public static void main(String args[]){
+        int a[]={1,2,3,4,5,6,43,2};
+        for(int n:a){
+            System.out.print(" "+n);
+        }
+    }
+}
