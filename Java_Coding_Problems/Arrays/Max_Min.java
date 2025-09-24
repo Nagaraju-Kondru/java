9242e61338fec747270602bb885d0237aba65022
Nagaraju.Kondru <nagaraju.kondru22@gmail.com>
2025-09-24 13:08:58 +0530

    Find the maximum & Minimum element in an array.

---
+++ Java_Coding_Problems/Arrays/Max_Min.java
@@ -0,0 +1,18 @@
+
+public class Max_Min {
+    public static void main(String args[]){
+        int a[]={1,2,3,4,5};
+        int max=a[0];
+        int min=a[0];
+        for(int i=1;i<a.length;i++){
+            if(a[i]>max){
+                max=a[i];
+            }
+            if(a[i]<min){
+                min=a[i];
+            }
+        }
+        System.out.println("Maximum: "+max);
+        System.out.println("Minimum: "+min);
+    }
+}
