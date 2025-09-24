07577e9ed452e6ae71929c29e2acc3fd5d07f044
Nagaraju.Kondru <nagaraju.kondru22@gmail.com>
2025-09-24 12:56:16 +0530

    Sum of all elements in an array.

---
+++ Java_Coding_Problems/Arrays/ArraySum.java
@@ -0,0 +1,10 @@
+public class ArraySum {
+    public static void main(String args[]){
+        int a[]={1,2,4,5,6,7,8};
+        int sum=0;
+        for(int n:a){
+            sum+=n;
+        }
+        System.out.println(sum);
+    }
+}
