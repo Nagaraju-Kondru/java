e9c54edbd277e2f82638812e55040b4064d9d56a
Nagaraju.Kondru <nagaraju.kondru22@gmail.com>
2025-09-25 17:23:52 +0530

    Find the sum of all subarrays.  O(n)

---
+++ Java_Coding_Problems/Arrays/SumOfAllSubarrays1.java
@@ -0,0 +1,10 @@
+public class SumOfAllSubarrays1 {
+    public static void main(String args[]){
+        int a[]={1,2,3};
+        int sum=0;
+        for(int i=0;i<a.length;i++){
+            sum+=a[i]*(i+1)*(a.length-i);
+        }
+        System.out.println("Sum: "+sum);
+    }
+}
